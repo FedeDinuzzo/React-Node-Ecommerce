d50485eca861ab7acac28bb22a8b499c66017b32
Fede Di Nuzzo <federicodinuzzo98@gmail.com>
2023-07-27 19:37:06 -0300

    vercel.json

--- src/index.js
+++ src/index.js
@@ -45,13 +45,13 @@ const upload = multer({ storage: storage })
 
 const app = express(); 
 
-// Middleware para habilitar CORS
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  next();
-});
+// // Middleware para habilitar CORS
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   res.setHeader('Access-Control-Allow-Credentials', 'true');
+//   next();
+// });
 
 
 // Define los middleware para la aplicación
